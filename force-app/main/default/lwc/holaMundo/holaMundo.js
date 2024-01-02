9446840ad00ba64f661065a69b8b68729185712c
Sebastian Claros <sclaros@gmail.com>
2024-01-02 10:17:10 -0300

    showerror

--- force-app/main/default/lwc/holaMundo/holaMundo.js
+++ force-app/main/default/lwc/holaMundo/holaMundo.js
@@ -3,7 +3,7 @@ import { LightningElement, track, api } from 'lwc';
 export default class HolaMundo extends LightningElement {
     @api recordId;
     @api titulo;
-    @track test;
+    @track errorMessage = 'mostrar error';
 
 
 }
---
+++ force-app/main/default/lwc/showError/showError.js
@@ -0,0 +1,33 @@
+import { LightningElement, api, track } from 'lwc';
+
+export default class ShowError extends LightningElement {
+
+    severity = ['SIN TITULO'];
+    @api message = 'test';
+
+    constructor() {
+        super();
+        console.log('constructor',this.severity);
+    }
+
+    renderedCallback() {
+        console.log('me rerenderize',this.severity);
+    }
+
+    connectedCallback() {
+        console.log('me connecte al dom',this.severity);
+    }
+
+    get firstSeverity() {
+        return this.severity[0];
+    }
+
+    changeError2(){
+        this.message = 'test 2';
+    }    
+    changeError(){
+        this.severity[0] = 'ERROR';
+        console.log('cambie el severity',this.severity);
+    }
+   
+}
