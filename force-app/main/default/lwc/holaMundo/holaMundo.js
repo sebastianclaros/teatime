db8e7f2022f592f3532bb9adcb72ca6536584d1d
Sebastian Claros <sclaros@gmail.com>
2024-01-05 17:59:35 -0300

    wire decorator

--- force-app/main/default/lwc/holaMundo/holaMundo.js
+++ force-app/main/default/lwc/holaMundo/holaMundo.js
@@ -1,9 +1,39 @@
-import { LightningElement, track, api } from 'lwc';
+import { LightningElement, track, api, wire } from 'lwc';
+import { getRecord } from 'lightning/uiRecordApi';
+import NAME_FIELD from "@salesforce/schema/Auto__c.Name";
+
 
 export default class HolaMundo extends LightningElement {
     @api recordId;
     @api titulo;
     @track errorMessage = 'mostrar error';
+    @track autoRecord;
 
+    @wire (getRecord, { recordId: '$recordId', fields: [NAME_FIELD, 'Auto__c.Marca__c']  } )
+    recordCallback( { error, data} ) {
+        if ( error ) {
+            console.error(error);
+        }
+        if ( data ) {
+            this.autoRecord = data.fields;
+        } 
+    };
+
+    items = [
+        { value: 'naranja'},
+        { value: 'verde'},
+        { value: 'azul'},
+        { value: 'blanco'}
+    ];
+    
+
+    get indexedItems() {
+        return this.items.map( (item, index) => {
+            return {...item, key: index};
+        })
+    }
+    get showTitle() {
+        return this.titulo != '' && this.recordId != '';
+    }
 
 }
