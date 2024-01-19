b8364a781f205e73df3791b81e84c6da357ead1b
Sebastian Claros <sclaros@gmail.com>
2024-01-19 18:00:28 -0300

    filtro

---
+++ force-app/main/default/lwc/filtroAutos/filtroAutos.js
@@ -0,0 +1,3 @@
+import { LightningElement } from 'lwc';
+
+export default class FiltroAutos extends LightningElement {}
--- force-app/main/default/lwc/grillaAutos/grillaAutos.js
+++ force-app/main/default/lwc/grillaAutos/grillaAutos.js
@@ -1,13 +1,20 @@
-import { LightningElement, wire, track } from 'lwc';
+import { LightningElement, wire, track , api} from 'lwc';
 import getAutos from '@salesforce/apex/AutoController.getAutos';
 
 export default class GrillaAutos extends LightningElement {
     @track autos = [];
+    @api marca = 'VW';
+    isError = false;
+    isLoading = true;
 
-    @wire (getAutos) autoCallback({data,error}) {
+    @wire (getAutos, { marca: '$marca'} ) autoCallback({data,error}) {
         console.log(error, data);
+        setTimeout( () => this.isLoading = false, 500 );
         if( data) {
             this.autos = data;
+        }
+        if ( error ) {
+            this.isError = true;
         }
     }
 
