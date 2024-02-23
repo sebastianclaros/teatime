310ac24edb7d7ac39889658ebf60bff7196e6036
Sebastian Claros <sclaros@gmail.com>
2024-02-23 18:38:06 -0300

    picklist de marcas

--- force-app/main/default/lwc/filtroAutos/filtroAutos.js
+++ force-app/main/default/lwc/filtroAutos/filtroAutos.js
@@ -1,3 +1,33 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track , wire} from 'lwc';
+import { getObjectInfo, getPicklistValues } from "lightning/uiObjectInfoApi";
+import AUTO_OBJECT from "@salesforce/schema/Auto__c";
+import MARCA_FIELD from '@salesforce/schema/Auto__c.Marca__c';
 
-export default class FiltroAutos extends LightningElement {}
+
+export default class FiltroAutos extends LightningElement {
+
+    @track marcas;
+    recordTypeId;
+
+    @wire(getObjectInfo, { objectApiName: AUTO_OBJECT })
+    results({ error, data }) {
+      if (data) {
+        this.recordTypeId = data.defaultRecordTypeId;
+      }
+    }
+  
+
+    @wire(getPicklistValues, { recordTypeId: "$recordTypeId", fieldApiName: MARCA_FIELD })
+    picklistResults({ error, data }) {
+    console.log( error, data);
+      if (data) {
+        this.marcas = data.values;
+
+      } else if (error) {
+      }
+    }
+
+    handleChangeMarca(e) {
+        console.log(e);
+    }
+}
