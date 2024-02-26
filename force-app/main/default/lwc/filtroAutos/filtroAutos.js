0115fa45310aa41ca5c6536f4222ec2a4d6bd751
Sebastian Claros <sclaros@gmail.com>
2024-02-26 18:22:34 -0300

    cambio a una api del getMarcas

--- force-app/main/default/lwc/filtroAutos/filtroAutos.js
+++ force-app/main/default/lwc/filtroAutos/filtroAutos.js
@@ -1,14 +1,27 @@
 import { LightningElement, track , wire} from 'lwc';
+import getMarcas from '@salesforce/apex/AutoController.getMarcas';
+/* Alternativa usando wire standard
 import { getObjectInfo, getPicklistValues } from "lightning/uiObjectInfoApi";
 import AUTO_OBJECT from "@salesforce/schema/Auto__c";
 import MARCA_FIELD from '@salesforce/schema/Auto__c.Marca__c';
-
+*/
 
 export default class FiltroAutos extends LightningElement {
 
     @track marcas;
-    recordTypeId;
 
+    @wire (getMarcas ) autoCallback({data,error}) {
+      if( data) {
+          this.marcas = data;
+      }
+      console.log( JSON.stringify(data) );
+  }
+  
+
+
+/* Alternativa usando wire standard
+    recordTypeId;
+    
     @wire(getObjectInfo, { objectApiName: AUTO_OBJECT })
     results({ error, data }) {
       if (data) {
@@ -26,6 +39,7 @@ export default class FiltroAutos extends LightningElement {
       } else if (error) {
       }
     }
+*/     
 
     handleChangeMarca(e) {
         console.log(e);
