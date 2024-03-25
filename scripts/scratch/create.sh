0d69780145d72c32a75c7975fe8623bfbffed3e0
Sebastian Claros <sclaros@gmail.com>
2024-03-25 18:57:29 -0300

    fix scripts

--- scripts/scratch/create.sh
+++ scripts/scratch/create.sh
@@ -2,12 +2,12 @@
 # Script crea scracth Org
 
 if [ -z "$1" ]; then  
+    echo "Falta el nombre del alias de org, generalmente el nombre del feature a realizar"
+else 
     sf org create scratch --set-default --definition-file=config/project-scratch-def.json --duration-days=7 --alias=$1
     sf project deploy start
     sf org assign permset --name=adminCatalogo
     sf data tree import --plan=data/plan.json
     sf open org
-else 
-    echo "Falta el nombre del alias de org, generalmente el nombre del feature a realizar"
 fi
 
