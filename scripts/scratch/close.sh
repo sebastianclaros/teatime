024fae46d8c83423c80e5affe30f0265ec672fb1
Sebastian Claros <sclaros@gmail.com>
2024-04-03 18:44:11 -0300

    fix

--- scripts/scratch/close.sh
+++ scripts/scratch/close.sh
@@ -34,3 +34,25 @@ if ! git push origin $current_branch; then
     echo "No se pudo publicar la branch"
     exit 1
 fi
+
+current_branch=$(git branch --show-current)
+
+# Baja lo ultimo de main y hace un merge 
+if ! git checkout main; then
+    echo "Verifique que no tenga cosas sin comitear"
+    exit 1
+fi
+if ! git pull; then
+    echo "Verifique que no tenga cosas sin comitear"
+    exit 1
+fi
+if ! git  merge --no-ff $current_branch; then
+    echo "Verifique que no tenga cosas sin comitear"
+    exit 1
+fi
+
+
+# if ! git branch -d $current_branch; then
+#     echo "No se pudo eliminar la branch"
+#     exit 1
+# fi
