9263cbf09e7b0eff7be6337ddb2c570eed369ce8
Marcos Gustavo Calderon <159566473+mgcalderonTeco@users.noreply.github.com>
2024-04-08 18:09:25 -0300

    Se cambio el formato del slider y se agrega Toggle de stock.

--- force-app/main/default/lwc/productFilter/productFilter.js
+++ force-app/main/default/lwc/productFilter/productFilter.js
@@ -13,6 +13,17 @@ export default class ProductFilter extends LightningElement {
 
   @track catalogo;
   @track categoria;
+  @track minRange = 30;
+  @track maxRange = 100;
+
+    handleInput(event) {
+        this[event.target.name] = event.target.value;
+        if (event.target.name === 'minRange' && this.minRange > this.maxRange) { 
+            this.minRange = this.maxRange;   
+        } else if (event.target.name === 'maxRange' && this.minRange > this.maxRange) {
+            this.maxRange = this.minRange;     
+        } 
+    }
 
   @wire(getCategorias, { catalogId: "$catalogo" }) categoriasCallback({
     data
