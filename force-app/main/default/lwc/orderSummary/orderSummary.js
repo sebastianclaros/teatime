29bbc3516a4d51e3557e57a4e8be8945369e9382
Sebastian Claros <sclaros@gmail.com>
2024-04-15 17:44:37 -0300

    label

--- force-app/main/default/lwc/orderSummary/orderSummary.js
+++ force-app/main/default/lwc/orderSummary/orderSummary.js
@@ -1,3 +1,14 @@
-import { LightningElement } from "lwc";
+import { LightningElement, wire } from "lwc";
+import ORDER from "@salesforce/schema/Order";
+import { getObjectInfo } from "lightning/uiObjectInfoApi";
 
-export default class OrderSummary extends LightningElement {}
+export default class OrderSummary extends LightningElement {
+  labels = {};
+
+  @wire(getObjectInfo, { objectApiName: ORDER })
+  oppInfo({ data }) {
+    if (data) {
+      this.labels.orderNumber = data.fields.ordeNumber.label;
+    }
+  }
+}
