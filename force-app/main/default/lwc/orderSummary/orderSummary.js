92e7a8f7c36cf8db3c095f80387eb076b6bded99
Sebastian Claros <sclaros@gmail.com>
2024-04-15 19:20:30 -0300

    label example

--- force-app/main/default/lwc/orderSummary/orderSummary.js
+++ force-app/main/default/lwc/orderSummary/orderSummary.js
@@ -1,14 +1,15 @@
-import { LightningElement, wire } from "lwc";
+import { LightningElement, track, wire } from "lwc";
 import ORDER from "@salesforce/schema/Order";
 import { getObjectInfo } from "lightning/uiObjectInfoApi";
 
 export default class OrderSummary extends LightningElement {
-  labels = {};
+  @track labels = {};
 
   @wire(getObjectInfo, { objectApiName: ORDER })
-  oppInfo({ data }) {
+  orderGetInfo({ data }) {
     if (data) {
-      this.labels.orderNumber = data.fields.ordeNumber.label;
+      //this.labels.orderNumber = data.fields.OrderNumber?.label;
+      this.labels.orderNumber = data.fields.OrderNumber?.inlineHelp;
     }
   }
 }
