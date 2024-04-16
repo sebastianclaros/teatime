69253d2e01b0fd49a5eaecedbb0a9ea6502e427b
Sebastian Claros <sclaros@gmail.com>
2024-04-16 18:23:53 -0300

    comments

--- scripts/scratch/close.sh
+++ scripts/scratch/close.sh
@@ -16,12 +16,14 @@
 current_branch=$(git branch --show-current)
 # Baja lo ultimo de main 
 
+# Fetch de origin (baja y actualiza de github la info del repo remoto)
 git fetch origin
 if [ $? -ne 0 ]; then
     echo "No pudo bajar lo ultimo"
     exit 1
 fi
 
+# Mueve la linea del tiempo como si esta branch nace de main de ahora asi resuelve conflicto localmente
 git rebase origin/main
 if [ $? -ne 0 ]; then
     echo "No se pudo hacer el rebase de main. Si hay conflictos resuelvalos y continue manualmente"
