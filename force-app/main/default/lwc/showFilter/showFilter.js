1fb28ab982d1241c1f8f33dd513001eddb910fef
Pablo Andersen <90287403+pablo-andersen@users.noreply.github.com>
2024-04-25 18:30:14 -0300

    creacion del componente showFilter

---
+++ force-app/main/default/lwc/showFilter/showFilter.js
@@ -0,0 +1,66 @@
+import { LightningElement, wire, track  } from 'lwc';
+import getProducts from "@salesforce/apex/ProductController.getProducts";
+import {
+  subscribe,
+  unsubscribe,
+  APPLICATION_SCOPE,
+  MessageContext
+} from "lightning/messageService";
+import channelProductFilter from "@salesforce/messageChannel/ProductFilter__c";
+
+export default class ShowFilter extends LightningElement {
+
+  @track productos = [];
+  catalogo;
+  categoria;
+  isError = false;
+  isLoading = true;
+  subscription;
+
+  @wire(MessageContext) messageContext;
+
+//   @wire(getProducts, { catalogId: "$catalogo", categoryId: "$categoria" })
+//   autoCallback({ data, error }) {
+//     console.log(data, error);
+//     this.isLoading = false;
+//     if (data) {
+//       this.productos = data.map((producto, index) => {
+//         return { key: `auto-key-${index}`, ...producto };
+//       });
+//     }
+//     if (error) {
+//       console.log(error);
+//       this.isError = true;
+//     }
+//   }
+
+  connectedCallback() {
+    this.subscribeToMessageChannel();
+  }
+
+  subscribeToMessageChannel() {
+    if (!this.subscription) {
+      this.subscription = subscribe(
+        this.messageContext,
+        channelProductFilter,
+        (message) => this.handleMessage(message),
+        { scope: APPLICATION_SCOPE }
+      );
+    }
+  }
+
+  handleMessage(payload) {
+    console.log(payload);
+    this.catalogo = payload.catalogo;
+    this.categoria = payload.categoria;
+  }
+
+  disconnectedCallback() {
+    this.unsubscribeToMessageChannel();
+  }
+
+  unsubscribeToMessageChannel() {
+    unsubscribe(this.subscription);
+    this.subscription = null;
+  }
+}
