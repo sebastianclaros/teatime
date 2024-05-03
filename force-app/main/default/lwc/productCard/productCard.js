e44200f425d1b1283c2c541d5539e43579348b91
ENRIQUE COREMBERG <enriqcore@gmail.com>
2024-05-03 11:17:44 -0300

    para debuggear el productCard en el navegador

--- force-app/main/default/lwc/productCard/productCard.js
+++ force-app/main/default/lwc/productCard/productCard.js
@@ -1,9 +1,13 @@
 import { LightningElement, api } from 'lwc';
 
+
 export default class CardItem extends LightningElement {
 
 
+@api record;
 
-
+connectedCallback() {
+console.log("esto es record----->", this.record);
+}
 
 }
