9ecd5c3225c6d8d9ffa58989ba306e2dac903085
Sebastian Claros <sclaros@gmail.com>
2024-05-03 18:24:15 -0300

    Req17 (#24)
    * FilterHelp
    
    * Adaptación de getProduct y FilterHelper
    
    * retoques ProductController y FilterHelper
    
    * fix terms
    
    * create skip if branch is created
    
    * Retoques ProductController y FilterHelper
    
    * test class y fix
    
    ---------
    
    Co-authored-by: Pablo Andersen <90287403+pablo-andersen@users.noreply.github.com>

--- scripts/scratch/create.sh
+++ scripts/scratch/create.sh
@@ -8,24 +8,28 @@ else
     dias="$2"
 fi
 
+
 if [ -z "$1" ]; then  
     echo "Falta el nombre del alias de org, generalmente el nombre del feature a realizar"
 else 
-    # Crea la branch nueva
-    git checkout main    
-    if [ $? -ne 0 ]; then
-        echo "Verifique que no tenga cosas sin comitear, no se puede crear una branch con cambios sin commitear, o bien los debera deshacer"
-        exit 1
-    fi
-    git pull
-    if [ $? -ne 0 ]; then
-        echo "Verifique que no tenga cosas sin comitear"
-        exit 1
-    fi
-    git checkout -b $1
-    if [ $? -ne 0 ]; then
-        echo "Verifique que no tenga cosas sin comitear"
-        exit 1
+    current_branch=$(git branch --show-current)
+    if [ $current_branch != $1 ]; then
+        # Crea la branch nueva
+        git checkout main    
+        if [ $? -ne 0 ]; then
+            echo "Verifique que no tenga cosas sin comitear, no se puede crear una branch con cambios sin commitear, o bien los debera deshacer"
+            exit 1
+        fi
+        git pull
+        if [ $? -ne 0 ]; then
+            echo "Verifique que no tenga cosas sin comitear"
+            exit 1
+        fi
+        git checkout -b $1
+        if [ $? -ne 0 ]; then
+            echo "Verifique que no tenga cosas sin comitear"
+            exit 1
+        fi
     fi
     # Crea la scracth org
     sf org create scratch --set-default --definition-file=config/project-scratch-def.json --duration-days=$dias --alias=$1
